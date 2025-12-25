93cc22b87d73c1ef5528cce3358dd8784cfde93a
Sayofsea <zfhelgjf-231218e@outlook.com>
2025-12-25 17:08:22 +0800

    secod vc

--- wuwu.c
+++ wuwu.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 {
-
+    int a=0;
+    if(a!=0)
+    {
+        printf("w");
+    }
+    else
+    {
+        printf("%d",a);
+    }
         printf("Hello world");
     return 0;
 }
